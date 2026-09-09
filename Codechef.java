19cf693e7e357b1533e93068a2aa59dd046ccdcc
Himanshu90909 <2.29582617e+08+Himanshu90909@users.noreply.github.com>
2026-09-09 16:54:40 +0000

    Simplify device control Codechef submission

--- Codechef.java
+++ Codechef.java
@@ -1,100 +1,69 @@
 // ================= DEVICE =================
-/*
-Abstraction:
-
-- Device is abstract
-- Device maintains ON/OFF state
-- turnOn() and turnOff() are abstract operations
-*/
 abstract class Device {
-    private boolean on;
+    private boolean on = false;
 
     public abstract void turnOn();
 
     public abstract void turnOff();
 
-    protected final boolean isOn() {
+    protected boolean isOn() {
         return on;
     }
 
-    protected final void setOn() {
-        on = true;
-    }
-
-    protected final void setOff() {
-        on = false;
+    protected void setOn(boolean value) {
+        on = value;
     }
 }
 
-
 // ================= FAN =================
-/*
-Inheritance:
-
-- Fan extends Device
-- Prints "Fan ON" and "Fan OFF"
-*/
 class Fan extends Device {
     @Override
     public void turnOn() {
-        if (isOn()) {
-            return;
+        if (!isOn()) {
+            setOn(true);
+            System.out.println("Fan ON");
         }
-        setOn();
-        System.out.println("Fan ON");
     }
 
     @Override
     public void turnOff() {
-        if (!isOn()) {
-            return;
+        if (isOn()) {
+            setOn(false);
+            System.out.println("Fan OFF");
         }
-        setOff();
-        System.out.println("Fan OFF");
     }
 }
 
-
 // ================= LIGHT =================
-/*
-- Light extends Device
-- Prints "Light ON" and "Light OFF"
-*/
 class Light extends Device {
     @Override
     public void turnOn() {
-        if (isOn()) {
-            return;
+        if (!isOn()) {
+            setOn(true);
+            System.out.println("Light ON");
         }
-        setOn();
-        System.out.println("Light ON");
     }
 
     @Override
     public void turnOff() {
-        if (!isOn()) {
-            return;
+        if (isOn()) {
+            setOn(false);
+            System.out.println("Light OFF");
         }
-        setOff();
-        System.out.println("Light OFF");
     }
 }
 
-
 // ================= CONTROLLER =================
 class DeviceController {
-
-    void operate(Device device) {
+    public void operate(Device device) {
         device.turnOn();
         device.turnOff();
     }
 }
 
-
 // ================= MAIN =================
 public class Codechef {
     public static void main(String[] args) {
-
         DeviceController controller = new DeviceController();
 
         Device fan = new Fan();
